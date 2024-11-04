1674e898dbf3a8b69e365ab45bb48cf86efcc9ca
Chatos_Xeno <koonsunch@gmail.com>
2024-11-04 16:39:03 +0700

    Change Handler to make it more stable

--- ProjectOOP/src/Handler.java
+++ ProjectOOP/src/Handler.java
@@ -1,14 +1,17 @@
 import java.awt.Graphics;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
- *
- * @author USER
+ * Handler class for managing all game objects.
+ * It includes methods for updating, rendering, adding, and removing game objects.
  */
 public class Handler {
     
     LinkedList<GameObject> object = new LinkedList<GameObject>();
-    
+
+    List<GameObject> toRemove = new LinkedList<>();
+
     private boolean up = false, down = false, right = false, left = false;
     
     public LinkedList<GameObject> getObject() {
@@ -18,7 +21,6 @@ public class Handler {
     public void clearObjects() {
         object.clear();
     }
-
 
     public void setObject(LinkedList<GameObject> object) {
         this.object = object;
@@ -55,28 +57,34 @@ public class Handler {
     public void setLeft(boolean left) {
         this.left = left;
     }
-    
-    public void tick(){
-        for(int i = 0; i < object.size();i++){
+
+    public void tick() {
+        for (int i = 0; i < object.size(); i++) {
             GameObject tempObject = object.get(i);
-            
             tempObject.tick();
         }
+
+        object.removeAll(toRemove);
+        toRemove.clear();
     }
     
-    public void render(Graphics g){
-        for(int i = 0; i < object.size();i++){
+
+    public void render(Graphics g) {
+        for (int i = 0; i < object.size(); i++) {
             GameObject tempObject = object.get(i);
-            
             tempObject.render(g);
         }
+
+        object.removeAll(toRemove);
+        toRemove.clear();
     }
     
-    public void addObject(GameObject tempObject){
+    public void addObject(GameObject tempObject) {
         object.add(tempObject);
     }
     
-    public void removeObject(GameObject tempObject){
-        object.remove(tempObject);
+
+    public void removeObject(GameObject tempObject) {
+        toRemove.add(tempObject);
     }
 }
